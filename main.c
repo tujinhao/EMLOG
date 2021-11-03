f3cee05d09004010bddefd2568326b0b0defea1e
tujinhao <author@1205228789@qq.com>
2021-11-03 20:40:37 +0800

    modify

--- main.c
+++ main.c
@@ -1,47 +1,12 @@
 #include <stdio.h>
 #include "log.h"
-typedef struct {
-	int status;
-	void (*statusChange)();    
-	
-}T_Device;
-T_Device g_Device;
-
-void addCallbackFunc(void (*pstatusChange)(int)) {
-
-	g_Device.statusChange = pstatusChange;
-
-}
-//int getStatus() {
-//
-//	return g_Device.status;
-//}
-void run() {
-
-
-	g_Device.status = 10;
-	if (g_Device.status == 10) {
-		if (g_Device.statusChange != NULL) {
-			g_Device.statusChange(g_Device.status);
-		}
-	}
-}
-//�û�����
-void callBack(int status) {
-	//EM_LOG(LOG_DEBUG, "callback start");
-	printf("status = %d\n", status);
-}
 
 int main() {
 	const int a = 123,b=321 ;
 
-	//EMLOG(LOG_WARN, "a = %d",a);
+	EMLOG(LOG_WARN, "a = %d",a);
 
-	EMLOG(LOG_INFO,"a = %d",a);
-
-	//int status = getStatus();  //��Ҫһֱ��ѯ
-	addCallbackFunc(callBack);   //�ص�����
-	run();
+	EMLOG(LOG_INFO,"b = %d",b);
 	return 0;
 	
 
